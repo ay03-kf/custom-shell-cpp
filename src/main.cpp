1639f7e63bd8093b9b83980fedda52055cb44362
Abir <abiryadav.2006@gmail.com>
2026-07-09 21:07:49 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,22 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   while(1){
-  // cout << "$ ";
-  // string s;
-  // getline(cin, s);
-  // if(s.length()>=5 && s.substr(0,5)=="echo "){
-  //   cout<< s.substr(5)<<endl;
-  // }
-  // else if(s=="exit") break;
-  // else {cout<<s<<": "<<"command not found\n";}
-  cout<<"$ ";
+  cout << "$ ";
   string s;
-  getline(cin,s);
+  getline(cin, s);
+  if(s=="exit") break;
+  else if(s.length()>=5 && s.substr(0,5)=="echo "){
+    cout<< s.substr(5)<<endl;
+  }
+  else if(s=="exit") break;
+  
+  else if(s.length()>=5 && s.substr(0,5)=="type "){
   string cmd=s.substr(5);
   if(cmd=="echo" || cmd=="type" || cmd=="exit"){
     cout<<cmd<<" is a shell builtin\n";
   }
-  else cout<<cmd<<": not found\n";
-  
+  else cout<<cmd<<": not found\n";}
+  else {cout<<s<<": "<<"command not found\n";
 }
 
 }
